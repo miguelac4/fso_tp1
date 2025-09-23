076a1af44453a1a6a9789674bc331928807313ca
miguelac4 <miguel.andre.cordeiro@gmail.com>
2025-09-23 14:41:15 +0100

    Test again

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	
 	public void run() {
 		
-		System.out.println("A aplicação começouu.");
+		System.out.println("A aplicação começou.");
 		while (!gui.getBD().isTerminar()) {
 			try {
 				Thread.sleep(100);
