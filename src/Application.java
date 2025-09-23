ff122d05d61792eccd97b62c3498d31d7ac61dc0
Ricardo <rtsousa.f@gmail.com>
2025-09-23 17:09:14 +0100

    gsgsg

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	
 	public void run() {
 		
-		System.out.println("A aplicação começouuu.");
+		System.out.println("A aplicação começoU.");
 		while (!gui.getBD().isTerminar()) {
 			try {
 				Thread.sleep(100);
