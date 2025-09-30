3f1b6950e705df0a7c13167c4c349b5dfd4fb5c3
miguelac4 <miguel.andre.cordeiro@gmail.com>
2025-09-30 16:42:25 +0100

    dada
    dadada

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,7 @@ public class Application {
 	
 	public void run() {
 		
-		System.out.println("A aplicação começooooU.");
+		System.out.println("A aplicação começou.");
 		while (!gui.getBD().isTerminar()) {
 			try {
 				Thread.sleep(100);
