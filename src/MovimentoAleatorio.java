cf5a18d7d94c3da8319b35c10b0af6c83dd21d0d
miguelac4 <miguel.andre.cordeiro@gmail.com>
2025-10-07 17:16:42 +0100

    ffasa

---
+++ src/MovimentoAleatorio.java
@@ -0,0 +1,16 @@
+
+public class MovimentoAleatorio extends Thread{
+	
+	public MovimentoAleatorio() {
+		
+	}
+	
+	public void comandoAleatorio() {
+		
+	}
+	
+	public void sendBuffer() {
+		
+	}
+
+}
---
+++ src/Servidor.java
@@ -0,0 +1,29 @@
+public class Servidor {
+	private BufferCircular buffer;
+	private RobotLegoEV3 robot;
+	
+	public Servidor(){
+		buffer = new BufferCircular();
+		robot = new RobotLegoEV3();
+	}
+	
+	public void Reta() {
+		
+	}
+	
+	public void CurvarDireita() {
+		
+	}
+
+	public void CurvarEsquerda() {
+		
+	}
+	
+	public void Parar() {
+		
+	}
+	
+	public void executar() {
+		
+	}
+}
